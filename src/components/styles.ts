c425f669f5a20c467ba844ead59cc1e935d20d72
Imran Azam <imran.azam@and.digital>
2022-02-16 14:46:25 +0000

    created simple styled grid component

---
+++ src/components/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const GridStyled = styled.div`
+  background: red;
+  padding: 1rem;
+`;
+
+export const GridRowStyled = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const GridCellStyled = styled.div`
+  width: 3rem;
+  height: 3rem;
+  background: yellow;
+  padding: 0.5rem;
+  border: 1px solid blue;
+  margin: 0, 5rem;
+  display: flex;
+  justify-content: center;
+`;
