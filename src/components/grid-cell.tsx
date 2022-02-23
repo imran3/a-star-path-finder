c425c4192e0f9468df5ef7509d65feec44633261
Imran Azam <imran.azam@and.digital>
2022-02-23 18:00:12 +0000

    gridcell display directional arrows on path

--- src/components/grid-cell.tsx
+++ src/components/grid-cell.tsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { GridCellStyled } from './styles';
 
 export const GridCell = ({ cell, onClick }) => {
+  let [directionArrow, setDirectionArrow] = useState(null);
+  let dirArrows = {
+    left: '←',
+    right: '→',
+    up: '↑',
+    down: '↓',
+  };
+
+  // display/clear direction arrows
+  useEffect(() => {
+    if (!cell.cameFrom) {
+      setDirectionArrow(null);
+      return;
+    }
+
+    let cm = cell.cameFrom;
+    if (cm) {
+      let arrow = '';
+
+      let [cmX, cmY] = [...cm.split('-')];
+      cmX = parseInt(cmX, 10);
+      cmY = parseInt(cmY, 10);
+      if (cell?.x === cmX) {
+        arrow = cell?.y > cmY ? dirArrows.left : dirArrows.right;
+        setDirectionArrow(arrow);
+      }
+
+      if (cell.y === cmY) {
+        arrow = cell?.x > cmX ? dirArrows.up : dirArrows.down;
+        setDirectionArrow(arrow);
+      }
+    }
+  }, [cell, cell.cameFrom]);
+
   return (
     <GridCellStyled
       onClick={onClick}
       style={{ background: cell?.bgColor ? cell.bgColor : 'purple' }}
     >
-      <div> </div>
+      <div>{directionArrow ? directionArrow : '*'}</div>
       {/* <div>
         {cell.x} - {cell.y}
       </div>
