7edea4ea12fc9fc6fbd89d2d934a6f7d3caa3a14
Imran Azam <imranazam3@gmail.com>
2022-03-04 14:23:57 +0000

    footer copy update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,6 +2,8 @@ import { FooterStyled } from './styles';
 
 export const Footer = () => {
   return (
-    <FooterStyled>Made with ... By .... Source code: link repo</FooterStyled>
+    <FooterStyled>
+      Made with React, by Imran Azam - Source code: link repo
+    </FooterStyled>
   );
 };
