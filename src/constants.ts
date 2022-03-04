0708ca82e5e30f1e9e2b2601bc30930d8c08c19d
Imran Azam <imran.azam@and.digital>
2022-03-04 14:17:51 +0000

    exported colors into constants file

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export const colors = {
+  StarCommandBlue: '#2274a5',
+  KeyLime: '#e7eb90',
+  NaplesYellow: '#fadf63',
+  Goldenrod: '#e6af2e',
+  Wine: '#632b30',
+  OldBurgundy: '#4f3130',
+  Catawba: '#753742',
+  cellNeutral: '#FFF3DE',
+  startCell: '#34c2b1',
+  goalCell: '#b5f5b3',
+  pathCell: '#caf0f8',
+  mouseHoverCell: '#FAC7A7',
+};
